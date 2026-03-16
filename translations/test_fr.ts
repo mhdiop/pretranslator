4b3e0ea361784ea044e81440d7be237979a8d516
Mouhamed DIOP <mouhamed.diop@esp.sn>
2026-03-16 20:17:34 +0000

    Add TS test file

---
+++ translations/test_fr.ts
@@ -0,0 +1,17 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<TS version="2.1" language="FR">
+ <context>
+  <name>MainWindow</name>
+
+  <message>
+   <source>Hello world</source>
+   <translation type="unfinished"></translation>
+  </message>
+
+  <message>
+   <source>Open file</source>
+   <translation type="unfinished"></translation>
+  </message>
+
+ </context>
+</TS>
